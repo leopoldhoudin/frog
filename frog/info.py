87a8bda3755a4ccbd08582324d781f7879f97ab8
leopoldhoudin <leopold.houdin@gmail.com>
2023-01-18 20:57:57 +0100

    add basic structure of repository

---
+++ frog/info.py
@@ -0,0 +1,3 @@
+__version__ = '1.0.0-dev1'
+__author__ = 'Léopold Houdin'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,27 @@
+import re
+import codecs
+import os.path
+from setuptools import setup, find_packages
+
+
+def find_version():
+    with codecs.open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), f"frog/info.py"
+        ),
+        "r",
+    ) as file:
+        return re.search('__version__ = "(\d+\.\d+\.\d+)"', file.read()).group(
+            1
+        )
+
+
+setup(
+    name="frog",
+    version=find_version(),
+    description="Code examiner.",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={"console_scripts": ["frog=frog.main:main"]},
+)
+
